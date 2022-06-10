d88d04f97dbce7b2999d8c120e757e24555d5c7c
Artsiom Kaliaha <stakkato95@gmail.com>
2022-06-10 20:29:41 +0200

    Update log message 7

--- main.go
+++ main.go
@@ -25,6 +25,6 @@ func main() {
 		os.Getenv("APP_DB_NAME"),
 		config.AppConfig.PostgresService)
 
-	fmt.Println("server is listeningBBB")
+	fmt.Println("server is listeningCCC")
 	a.Run(":8010")
 }
