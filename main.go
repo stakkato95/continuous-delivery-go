0c569f7890f96891489ff10fe7a33e33ef4fd762
Artsiom Kaliaha <stakkato95@gmail.com>
2022-06-10 21:57:52 +0200

    Update log message 9

--- main.go
+++ main.go
@@ -25,6 +25,6 @@ func main() {
 		os.Getenv("APP_DB_NAME"),
 		config.AppConfig.PostgresService)
 
-	fmt.Println("server is listeningDDD")
+	fmt.Println("server is listeningEEE")
 	a.Run(":8010")
 }
